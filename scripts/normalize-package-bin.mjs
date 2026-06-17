60c9890479e75d7f2887833c233a7069653854f2
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2026-06-17 17:07:34 +0700

    chore: prepare 0.1.5 release

---
+++ scripts/normalize-package-bin.mjs
@@ -0,0 +1,10 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+const packageJsonPath = new URL('../package.json', import.meta.url);
+const packageJson = JSON.parse(await readFile(packageJsonPath, 'utf8'));
+
+packageJson.bin = {
+  parix: 'dist/cli.cjs',
+};
+
+await writeFile(packageJsonPath, `${JSON.stringify(packageJson, null, 2)}\n`);
